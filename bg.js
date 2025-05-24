ad63afc72eedaa453fc3fa43cfdac89c9aec69ef
a <106607480+ariamaranai@users.noreply.github.com>
2025-05-24 22:59:31 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -5,16 +5,19 @@ chrome.downloads.onCreated.addListener(item => {
   if (!item.byExtensionId) {
     let src = item.url;
     if (src[0] == "f") {
-      let n = src.length;
-      let c = src[n - 1];
-      (c == "v" || c == "V") && ((c = src[n - 3]) == "m" || c == "M") && src[n - 4] == "." && (
-        chrome.downloads.cancel(item.id),
-        chrome.storage.local.get("0", v =>
-          chrome.tabs.create({
-            url: (v[0] || "as.mp4.htm?") + src
-          })
+      let { id } = item;
+      chrome.downloads.pause(id);
+      let ext = src.slice(-4).toLowerCase();
+      ext == ".mov" || ext == ".m4v"
+        ? (
+          chrome.downloads.cancel(item.id),
+          chrome.storage.local.get("0", v =>
+            chrome.tabs.create({
+              url: (v[0] || "as.mp4.htm?") + src
+            })
+          )
         )
-      )
+        : chrome.downloads.resume(id)
     } 
   }
 });
