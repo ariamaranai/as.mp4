0f4adf5eefe46f5308959c753d219f2e1673a0a8
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-29 22:32:25 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,20 +1,13 @@
-chrome.omnibox.onInputEntered.addListener(s =>
-  chrome.storage.local.set({ 0: s + "?" })
-);
 chrome.downloads.onCreated.addListener(item => {
   if (!item.byExtensionId) {
     let src = item.url;
     if (src[0] == "f") {
-      let { id } = item;
+      let id = item.id;
       chrome.downloads.cancel(id);
       chrome.downloads.erase({ id });
       let ext = src.slice(-4).toLowerCase();
       (ext == ".mov" || ext == ".m4v") &&
-      chrome.storage.local.get("0", v =>
-        chrome.tabs.update({
-          url: (v[0] || "as.mp4.htm?") + src
-        })
-      );
+      chrome.tabs.update({ url: "as.mp4.htm?" + src });
     }
   }
 });
