34bbe31f0818b2d245608a798a38944b5fdbe4d6
a <106607480+ariamaranai@users.noreply.github.com>
2026-01-15 17:02:55 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,11 +1,11 @@
 chrome.downloads.onCreated.addListener(item => {
   if (!item.byExtensionId) {
-    let { url } = item;
-    if (url[0] == "f" && /\.m[o4]v$/i.test(url)) {
+    let { finalUrl } = item;
+    if (finalUrl[0] == "f" && /\.m[o4]v$/i.test(finalUrl)) {
       let { id } = item;
       chrome.downloads.cancel(id);
       chrome.downloads.erase({ id });
-      chrome.tabs.update({ url: "as.mp4.htm?" + url });
+      chrome.tabs.update({ url: "as.mp4.htm?" + finalUrl });
     }
   }
 });
