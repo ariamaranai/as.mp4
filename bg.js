810ae2ca45fd1d6529d6ee8d229b664adff129f4
a <106607480+ariamaranai@users.noreply.github.com>
2026-05-17 17:58:21 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,7 +1,15 @@
 chrome.downloads.onCreated.addListener(item => {
   if (!item.byExtensionId) {
     let { finalUrl } = item;
-    if (finalUrl[0] == "f" && /\.m[o4]v$/i.test(finalUrl)) {
+    let len = finalUrl.length;
+    let c;
+    if (
+      finalUrl.charCodeAt() == 102 &&
+      ((c = finalUrl.charCodeAt(len - 4)) == 118 || c == 86) &&
+      ((c = finalUrl.charCodeAt(len - 3)) == 52 || c == 111 || c == 79) &&
+      ((c = finalUrl.charCodeAt(len - 2)) == 109 || c == 77) &&
+      (c = finalUrl.charCodeAt(len - 1)) == 46
+    ) {
       let { id } = item;
       chrome.downloads.cancel(id);
       chrome.downloads.erase({ id });
